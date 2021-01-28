40294f7bd94a96b06fffc8937dbd6ef39a526727
Sheng Hao <haosheng0211@gmail.com>
2021-01-28 08:22:44 +0800

    配置頁面標題

--- views/src/app.vue
+++ views/src/app.vue
@@ -3,3 +3,19 @@
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  metaInfo() {
+    return {
+      title: this.title + "｜管理平台"
+    };
+  }
+})
+export default class App extends Vue {
+  get title() {
+    return this.$route.meta.title;
+  }
+}
+</script>
